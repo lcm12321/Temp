add159a0f26cec4e1c5f4abfa25d6aa55623943a
lcm6474 <lcm6474@17u.cn>
2014-01-23 10:12:07 +0800

    8676

--- 单元测试初阶/Program.cs
+++ 单元测试初阶/Program.cs
@@ -27,7 +27,7 @@ namespace 单元测试初阶
 
         static void Main(string[] args)
         {
-            //uiytsduifgsdfui
+            //uiytsduifgsdfui突突突
             string str="1,2,3,4,5,6,";
             Console.WriteLine(str.Substring(0,str.Length-1));
             Console.Read();
