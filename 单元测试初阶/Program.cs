517ae3e889bcec7ed66a3ee03436b5e6e2f81e94
lcm6474 <lcm6474@17u.cn>
2014-01-23 10:20:25 +0800

    8676

--- 单元测试初阶/Program.cs
+++ 单元测试初阶/Program.cs
@@ -27,7 +27,7 @@ namespace 单元测试初阶
 
         static void Main(string[] args)
         {
-            //uiytsduifgsdfui突突突
+            //uiytsduifgsdfui儿童热热特特
             string str="1,2,3,4,5,6,";
             Console.WriteLine(str.Substring(0,str.Length-1));
             Console.Read();
